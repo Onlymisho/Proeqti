bf8b8191e9f87e4acbc52a209a577dfaecf63ff4
Onlymisho <148981118+Onlymisho@users.noreply.github.com>
2024-03-24 20:31:09 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
+import { EndComponent } from './end/end.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    EndComponent
   ],
   imports: [
     BrowserModule,
